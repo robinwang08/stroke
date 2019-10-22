4fb62b7d30df53dd4b51d0a9e8c33384a509acfb
Robin <robinwang08@gmail.com>
2019-10-21 20:35:47 -0400

    Update strokeConfig.py

--- strokeConfig.py
+++ strokeConfig.py
@@ -1,32 +1,32 @@
 class Config(object):
     SEED = 123
-    CSV_FILE = './islesdata.csv'
+    CSV_FILE = './data.csv'
 
-    VALIDATION_SPLIT = .33
-    TEST_SPLIT = .30
+    VALIDATION_SPLIT = .30
+    TEST_SPLIT = .333
 
-    train_MTT = './examples/configFiles/stroke2/train/trainChannels_MTT.cfg'
-    train_rCBF = './examples/configFiles/stroke2/train/trainChannels_rCBF.cfg'
-    train_rCBV = './examples/configFiles/stroke2/train/trainChannels_rCBV.cfg'
-    train_Tmax = './examples/configFiles/stroke2/train/trainChannels_Tmax.cfg'
-    train_GT = './examples/configFiles/stroke2/train/trainGtLabels.cfg'
-    train_roi = './examples/configFiles/stroke2/train/trainRoiMasks.cfg'
+    train_MTT = './examples/configFiles/stroke/train/trainChannels_MTT.cfg'
+    train_rCBF = './examples/configFiles/stroke/train/trainChannels_rCBF.cfg'
+    train_rCBV = './examples/configFiles/stroke/train/trainChannels_rCBV.cfg'
+    train_Tmax = './examples/configFiles/stroke/train/trainChannels_Tmax.cfg'
+    train_GT = './examples/configFiles/stroke/train/trainGtLabels.cfg'
+    train_roi = './examples/configFiles/stroke/train/trainRoiMasks.cfg'
 
 
-    validation_MTT = './examples/configFiles/stroke2/train/validation/validationChannels_MTT.cfg'
-    validation_rCBF = './examples/configFiles/stroke2/train/validation/validationChannels_rCBF.cfg'
-    validation_rCBV = './examples/configFiles/stroke2/train/validation/validationChannels_rCBV.cfg'
-    validation_Tmax = './examples/configFiles/stroke2/train/validation/validationChannels_Tmax.cfg'
-    validation_GT = './examples/configFiles/stroke2/train/validation/validationGtLabels.cfg'
-    validation_pred = './examples/configFiles/stroke2/train/validation/validationNamesOfPredictions.cfg'
-    validation_roi = './examples/configFiles/stroke2/train/validation/validationRoiMasks.cfg'
+    validation_MTT = './examples/configFiles/stroke/train/validation/validationChannels_MTT.cfg'
+    validation_rCBF = './examples/configFiles/stroke/train/validation/validationChannels_rCBF.cfg'
+    validation_rCBV = './examples/configFiles/stroke/train/validation/validationChannels_rCBV.cfg'
+    validation_Tmax = './examples/configFiles/stroke/train/validation/validationChannels_Tmax.cfg'
+    validation_GT = './examples/configFiles/stroke/train/validation/validationGtLabels.cfg'
+    validation_pred = './examples/configFiles/stroke/train/validation/validationNamesOfPredictions.cfg'
+    validation_roi = './examples/configFiles/stroke/train/validation/validationRoiMasks.cfg'
 
-    test_MTT = './examples/configFiles/stroke2/test/testChannels_MTT.cfg'
-    test_rCBF = './examples/configFiles/stroke2/test/testChannels_rCBF.cfg'
-    test_rCBV = './examples/configFiles/stroke2/test/testChannels_rCBV.cfg'
-    test_Tmax = './examples/configFiles/stroke2/test/testChannels_Tmax.cfg'
-    test_GT = './examples/configFiles/stroke2/test/testGtLabels.cfg'
-    test_pred = './examples/configFiles/stroke2/test/testNamesOfPredictions.cfg'
-    test_roi = './examples/configFiles/stroke2/test/testRoiMasks.cfg'
+    test_MTT = './examples/configFiles/stroke/test/testChannels_MTT.cfg'
+    test_rCBF = './examples/configFiles/stroke/test/testChannels_rCBF.cfg'
+    test_rCBV = './examples/configFiles/stroke/test/testChannels_rCBV.cfg'
+    test_Tmax = './examples/configFiles/stroke/test/testChannels_Tmax.cfg'
+    test_GT = './examples/configFiles/stroke/test/testGtLabels.cfg'
+    test_pred = './examples/configFiles/stroke/test/testNamesOfPredictions.cfg'
+    test_roi = './examples/configFiles/stroke/test/testRoiMasks.cfg'
 
 config = Config()
